4c7f7cf7cad384f90cf2624dab4b7eb11e14dd5b
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-08-27 10:05:00 -0500

    Add files via upload

---
+++ BeatifulTriplets.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main()
+{
+    int n; std::cin >> n; //the length of the sequence
+    int d; std::cin >> d; //the beautiful difference
+    std::vector<int> vec;
+    vec.reserve(n);
+    std::copy_n(std::istream_iterator<int>(std::cin),
+            n, back_inserter(vec));
+
+    int count = 0;
+    for (int index = 0; index<=n-3; ++index)
+        for (int j = index+1; j<=n-2; ++j)
+            if(vec[j]-vec[index] == d)
+                for (int h = j+1; h<n; ++h)
+                    if(vec[h]-vec[j] == d)
+                        ++count;
+                    //std::cout << vec[index] <<" "<< vec[j]<<" "<< vec[h] <<std::endl;
+    std::cout << count << std::endl;
+    return 0;
+}
