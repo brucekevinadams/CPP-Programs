944256cdc9d93b4a6d5cbdcf382be4e78c0a7f29
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-09-04 08:09:09 -0500

    Add files via upload

---
+++ CommonChild.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the commonChild function below.
+int commonChild(string s1, string s2) {
+
+// matrix c stores the char count of child string
+vector < vector<int> > c(s1.length()+1,vector<int>(s2.length()+1));
+for (int i=1;i<=s1.length();i++) {
+    for (int j=1;j<=s2.length();j++) {        
+        if (s1[i-1]==s2[j-1]) c[i][j]=c[i-1][j-1]+1;
+        else c[i][j]=max(c[i][j-1], c[i-1][j]);
+    }
+} 
+
+return c[s1.length()][s2.length()]; 
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s1;
+    getline(cin, s1);
+
+    string s2;
+    getline(cin, s2);
+
+    int result = commonChild(s1, s2);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
