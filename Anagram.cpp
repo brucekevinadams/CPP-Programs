24c98c50849b8eb4faf5c54956a161aafc6d4e88
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-10-11 07:55:55 -0500

    Add files via upload

---
+++ Anagram.cpp
@@ -0,0 +1,31 @@
+#include < bits / stdc++.h >
+
+using namespace std;
+
+int main() {
+
+int q;
+cin >> q;
+
+for (int a0 = 0; a0 < q; a0++) {
+	string s;
+	cin >> s;
+
+	if (s.length() % 2) cout << -1 << endl;
+	else {
+		vector < int > s1(26);
+		vector < int > s2(26);
+		for (int i = 0; i < s.length() / 2; i++) {
+			s1[s[i] - 'a']++;
+			s2[s[i + s.length() / 2] - 'a']++;
+		}
+
+		int min = 0;
+		for (int i = 0; i < 26; i++)
+			if (s2[i] > s1[i]) min += (s2[i] - s1[i]);
+		cout << min << endl;
+	}
+}
+
+return 0;
+}
